ba8f15e617509782e9de4b01e82a7faac5101364
shafali03 <shaf03sm@gmail.com>
2019-03-19 11:29:37 +0000

    signinRegister

---
+++ backEndFaceDetect/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+
+//signIn
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success we did it this time');
+    } else {
+        res.status(400).json('error logging in please try to register again ');
+    }
+})
+
+//register to push to add to the user array
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1]);
+})
+
+app.listen(3000, () => {
+    console.log('apps is running on port 3000');
+})
+
+/*
+    / --> res = this is working
+    / signin --> POST = success/fail
+    /register --> POST = user
+    /profile/: userId --> GET user
+    /image --> PUT  --> user
+
+
+    */
